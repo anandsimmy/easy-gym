a6adeccb8ad71aa59eb73104823694854de43be8
Anand Simmy <anandsimmy7@gmail.com>
2020-09-11 15:31:08 +0530

    added programs section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
 import './styles.scss'
 
-const Header=() => {
+const Header=({ sideNav,changeSidenav }) => {
+
     return (
         <div className='navbar'>
+            <div className='mobile-hamburger'>
+                <i class="material-icons" onClick={()=>changeSidenav(true)}>menu</i>
+            </div>
+            { sideNav && 
+            <ul className='side-nav'> 
+                <li className='navbar-highlighted'>HOME</li>
+                <li>ABOUT</li>
+                <li>CLASSES</li>
+                <li>SCHEDULES</li>
+                <li>CONTACT</li>
+                <button className='sign-up'>
+                    SIGN UP
+                </button>
+            </ul> 
+            }
             <div className='left-items'>
                 TRAINING <span className='navbar-highlighted'>STUDIO</span>
             </div>
             <div className='right-items'>
-                    <div className='navbar-highlighted'>HOME</div>
-                    <div>ABOUT</div>
-                    <div>CLASSES</div>
-                    <div>SCHEDULES</div>
-                    <div>CONTACT</div>
+                    <span className='navbar-highlighted'>HOME</span>
+                    <span>ABOUT</span>
+                    <span>CLASSES</span>
+                    <span>SCHEDULES</span>
+                    <span>CONTACT</span>
                     <button className='sign-up'>
                         SIGN UP
                     </button>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../Components/Header/Header'
 import './styles.scss'
 
 const Home=() => {
+
+    const [sideNav, changeSidenav]= useState(false)
+
     return (
     <>
-    <Header />
-    <div className='main-section'>
+    <Header sideNav={sideNav} changeSidenav={changeSidenav} />
+    <div className='main-section' onClick={()=>changeSidenav(false)}>
         <div className='main-overlay'></div> 
-        <video width='100%' height='100%' autoPlay loop>
-            <source src="./gym.mp4" type="video/mp4" />
+        <video id='video' className='main-video' autoPlay loop muted>
+            <source src="assets/gym.mp4" type="video/mp4" />
         </video>
         <div className='info-section'>
             <span className='header'>
@@ -21,6 +24,124 @@ const Home=() => {
             <button className='member-button'>
                 BECOME A MEMBER
             </button>
+        </div>
+    </div>
+    <div className='choose-program'>
+        <div className='choose-program-main-section'>
+            CHOOSE <span className='choose-program-main-section-highlighted'>PROGRAM</span>
+            <div classname='choose-program-top-img'>
+                <img alt='lines' src='assets/line-dec.png' />
+            </div>
+            <div className='choose-program-info'>
+                Training studio is free CSS template for gyms and fitness centers. You are allowed to use this 
+                layoutfor your business website.
+            </div>
+        </div>
+        <div className='programs-section'>
+            <div className='program'>
+                <div className='program-img'>
+                    <img alt='program' src='assets/features-first-icon.png' />
+                </div>
+                <div className='program-info'>
+                    <span className='program-info-heading'>
+                        Basic Fitness
+                    </span>
+                    <span className='program-info-content'>
+                        Please donot redistribute this template zip file on any template
+                        collection website. Thisis not allowed. 
+                    </span>
+                    <span className='program-discover-more'>
+                        DISCOVER MORE
+                    </span>
+                </div>
+            </div>
+            <div className='program'>
+                <span className='program-img'>
+                    <img alt='program' src='assets/features-first-icon.png' />
+                </span>
+                <div className='program-info'>
+                    <div className='program-info-heading'>
+                        Advanced Muscle Course
+                    </div>
+                    <div className='program-info-content'>
+                        You may want ot browse through <span className='program-info-highlighted'>Digital Marketing</span> or 
+                        <span className='program-info-highlighted'> Corporate</span> HTML CSS Templates.
+                        on our website.
+                    </div>
+                    <div className='program-discover-more'>
+                        DISCOVER MORE
+                    </div>
+                </div>
+            </div>
+            <div className='program'>
+                <div className='program-img'>
+                    <img alt='program' src='assets/features-first-icon.png' />
+                </div>
+                <div className='program-info'>
+                    <span className='program-info-heading'>
+                        New Gym Training
+                    </span>
+                    <span className='program-info-content'>
+                    You may want ot browse through Digital Marketing or 
+                    Corporate HTML CSS Templates. 
+                    </span>
+                    <span className='program-discover-more'>
+                        DISCOVER MORE
+                    </span>
+                </div>
+            </div>
+            <div className='program'>
+                <div className='program-img'>
+                    <img alt='program' src='assets/features-first-icon.png' />
+                </div>
+                <div className='program-info'>
+                    <span className='program-info-heading'>
+                        Yoga Training
+                    </span>
+                    <span className='program-info-content'>
+                        Please donot redistribute this template zip file on any template
+                        collection website. Thisis not allowed. 
+                    </span>
+                    <span className='program-discover-more'>
+                        DISCOVER MORE
+                    </span>
+                </div>
+            </div>
+            <div className='program'>
+                <div className='program-img'>
+                    <img alt='program' src='assets/features-first-icon.png' />
+                </div>
+                <div className='program-info'>
+                    <span className='program-info-heading'>
+                        Basic Muscle Course
+                    </span>
+                    <span className='program-info-content'>
+                         Credit goes <span className='program-info-highlighted'>Pexels website</span> for images and video background used in this
+                         HTML template.
+                    </span>
+                    <span className='program-discover-more'>
+                        DISCOVER MORE
+                    </span>
+                </div>
+            </div>
+            <div className='program'>
+                <div className='program-img'>
+                    <img alt='program' src='assets/features-first-icon.png' />
+                </div>
+                <div className='program-info'>
+                    <span className='program-info-heading'>
+                        Body Building Course
+                    </span>
+                    <span className='program-info-content'>
+                        You may want ot browse through Digital Marketing or 
+                        Corporate HTML CSS Templates.  
+                    </span>
+                    <span className='program-discover-more'>
+                        DISCOVER MORE
+                    </span>
+                </div>
+            </div>
+
         </div>
     </div>
     </>)
